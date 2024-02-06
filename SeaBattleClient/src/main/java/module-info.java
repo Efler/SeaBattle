942e77435b1ce5daecf33c7d556a3951db1cfd45
eflerrr <cool.yan_sv@mail.ru>
2024-02-07 00:26:32 +0300

    modules added

--- SeaBattleClient/src/main/java/module-info.java
+++ SeaBattleClient/src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module org.eflerrr.seabattle.client {
     requires javafx.controls;
     requires javafx.fxml;
+    requires MaterialFX;
+    requires org.apache.commons.lang3;
 
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.bootstrapicons;
 
     opens org.eflerrr.seabattle.client to javafx.fxml;
     exports org.eflerrr.seabattle.client;
+    exports org.eflerrr.seabattle.client.controller;
+    opens org.eflerrr.seabattle.client.controller to javafx.fxml;
 }
